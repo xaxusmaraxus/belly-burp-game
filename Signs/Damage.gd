6d4ccd3b71b331c6880837c29a6c8f4e73ae11f8
Stefan <stefan@somnolik.at>
2023-02-13 11:55:49 +0100

    Fix typo

--- Signs/Damage.gd
+++ Signs/Damage.gd
@@ -1,10 +1,10 @@
 extends Node2D
 
 onready var damage_label = $DamageLabel
-onready var anim_playser = $AnimationPlayer
+onready var anim_player = $AnimationPlayer
 
 func _ready():
-	anim_playser.play("show")
+	anim_player.play("show")
 
 func _on_AnimationPlayer_animation_finished(anim_name):
 	if anim_name == "show":
