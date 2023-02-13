3c02c06442e888578571421cf7ee43d1da9ddbc5
Stefan <stefan@somnolik.at>
2023-02-13 11:30:15 +0100

    Remove enemies

--- Game/Game.gd
+++ Game/Game.gd
@@ -6,13 +6,6 @@ enum {
 	GAMEOVER
 }
 
-const enemy_scns = [
-	preload("res://Enemies/EnemyBeige.tscn"),
-	preload("res://Enemies/EnemyBlue.tscn"),
-	preload("res://Enemies/EnemyGreen.tscn"),
-	preload("res://Enemies/EnemyPink.tscn"),
-	preload("res://Enemies/EnemyYellow.tscn")
-]
 
 export (int) var power = 10
 var life: int = 10
@@ -20,7 +13,6 @@ var level: int = 1
 var state = READY
 var enemy #Inherited Enemy node
 
-onready var enemy_spawner = $EnemySpawner
 onready var ui = $UI
 onready var grid = $Grid
 onready var sign_screen = $Screens/SignScreen
@@ -39,15 +31,6 @@ func _ready():
 	sign_timer.start()
 
 
-func spawn_enemy():
-	var index = randi() % enemy_scns.size()
-	enemy = enemy_scns[index].instance()
-	enemy_spawner.call_deferred("add_child", enemy)
-	enemy.set_level(level)
-	enemy.connect("attack_done", self, "_on_Enemy_attack_done")
-	enemy.connect("tree_exited", self, "_on_Enemy_tree_exited")
-
-
 func _on_SignTimer_timeout():
 	match state:
 		READY:
@@ -56,7 +39,7 @@ func _on_SignTimer_timeout():
 		PLAY:
 			sign_timer.stop()
 			sign_screen.hide()
-			spawn_enemy()
+			# spawn_enemy()
 		GAMEOVER:
 			sign_screen.hide()
 			end_screen.show_result(level - 1)
@@ -82,20 +65,3 @@ func _on_Grid_waiting_finished(total_combo):
 	if enemy != null:
 		enemy.hit(power, total_combo)
 
-
-# Connected signal
-func _on_Enemy_attack_done():
-	life -= 1
-	ui.update_life(life)
-	if life <= 0:
-		lose()
-
-
-# Connected signal
-func _on_Enemy_tree_exited():
-	enemy = null
-	level += 1
-	ui.update_level(level)
-	power += level - int(randi() % level)
-	ui.update_power(power)
-	spawn_enemy()
